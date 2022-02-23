e615ee1ea724d0d634bb32451830c28486dea572
mgurga <motiurga@gmail.com>
2022-02-22 23:52:40 -0800

    add data and timer

---
+++ src/timer.svelte
@@ -0,0 +1,43 @@
+
+<script>
+    let time = 60; // in seconds
+    let countingdown = false;
+
+    function toggle() {
+        countingdown = !countingdown;
+    }
+
+    function reset() {
+        time = 60;
+        countingdown = false;
+    }
+
+    function addMin() {
+        time += 60;
+    }
+
+    let countinterval = setInterval(function() {
+        if (countingdown) time--;
+    }, 1000);
+</script>
+
+<style>
+    #timer {
+        background-color: black;
+        color: white;
+    }
+
+    p {
+        background-color: black;
+        border-radius: 5px;
+        padding-left: 10px;
+        padding-right: 10px;
+    }
+</style>
+
+<div id="timer">
+    <button on:click={toggle}>{countingdown ? "Stop" : "Start"}</button>
+    <button on:click={reset}>Reset</button>
+    <button on:click={addMin}>Add 1 Minute</button>
+    <p style="float: right; font-size: 28pt; margin: 0; color: red;">{time}</p>
+</div>
